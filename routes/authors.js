08404f250b0c72a76cf861f3f5b282c2b299c7f1
Harish Sapte <harish.sapte10@gmail.com>
2020-08-10 15:24:11 +0530

    Added author routes

--- routes/authors.js
+++ routes/authors.js
@@ -30,6 +30,7 @@ router.post("/", async (req, res) => {
 
   try {
     const newAuthor = await author.save();
+    res.redirect("authors");
   } catch {
     res.render("authors/new", {
       author: author,
